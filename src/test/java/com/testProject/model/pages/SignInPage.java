101d9471121b5dd4a2ea91e7d88a5bba5fff4f28
bvatrapu <bharat.vatrapu@outlook.com>
2019-08-31 11:16:18 +0530

    Objects added to SignIn Page

--- src/test/java/com/testProject/model/pages/SignInPage.java
+++ src/test/java/com/testProject/model/pages/SignInPage.java
@@ -19,6 +19,54 @@ public class SignInPage extends DriverBase {
         PageFactory.initElements(this.driver,this);
     }
 
+    @CacheLookup
+    @FindBy(xpath = "//img[@class='uca-header-logo-randalls']")
+    public WebElement imgRandalls;
+
+    @CacheLookup
+    @FindBy(xpath = "//h2[text()='Sign In']")
+    public WebElement textsignIn;
+
+    @CacheLookup
+    @FindBy(xpath = "//label[text()='Email Address']")
+    public WebElement textEmailAddress;
+
+    @CacheLookup
+    @FindBy(xpath = "//input[@id='label-email']")
+    public WebElement enterEmailAddress;
+
+    @CacheLookup
+    @FindBy(xpath = "//label[text()='Password']")
+    public WebElement textPassword;
+
+    @CacheLookup
+    @FindBy(xpath = "//input[@id='label-password']")
+    public WebElement enterPassword;
+
+    @CacheLookup
+    @FindBy(xpath = "//div[@id='cShow']")
+    public WebElement linkShow;
+
+
+    @CacheLookup
+    @FindBy(xpath = "//label[text()='Keep Me Signed In']")
+    public WebElement textKeepMeSignedIn;
+
+    @CacheLookup
+    @FindBy(xpath = "//p[text()='For your security, we do not recommend checking this box if you are using a public device.']")
+    public WebElement textSecurityWarning;
+
+    @CacheLookup
+    @FindBy(xpath = "//input[@id='btnSignIn']")
+    public WebElement btnSignIn;
+
+    @CacheLookup
+    @FindBy(xpath = "//li[@role='presentation']//a[text()='Create Account']")
+    public WebElement linkcreateAccount;
+
+    @CacheLookup
+    @FindBy(xpath = "//a[text()='Forgot Password']")
+    public WebElement linkforgotPassword;
 
 
 }
